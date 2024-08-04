91b762858be0a0345184caab7f15c2263582b3fc
knaut <daniel@smallinvisiblemachines.com>
2024-08-03 17:52:30 -0700

    basic css and index w/ node events stream

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
 import express from 'express'
 import * as osc from 'node-osc'
 
-console.log(osc)
+// console.log(osc)
 
 const app = express()
 app.use(express.static('public'))
-
-app.get('/countdown', function(req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
-  })
-  countdown(res, 10)
-})
 
 const vdmx = {
   ip: '192.168.0.30',
@@ -24,7 +15,24 @@ const oscServer = new osc.Server(vdmx.port, vdmx.ip, () => {
   console.log('OSC server listening')
 })
 
-oscServer.on('message', msg => console.log('osc message:', msg))
+app.get('/osc', function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  })
+  // countdown(res, 10)
+
+  oscServer.on('message', msg => {
+    console.log('osc message:', msg)
+
+    res.write("data: " + `${msg}` + "\n\n")
+  })
+})
+
+
+
+
 
 function countdown(res, count) {
   res.write("data: " + count + "\n\n")
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,14 @@
-<!-- <h1>Welcome to SvelteKit</h1> -->
-<!-- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
 <script>
-	/*import osc from '../../../node_modules/osc/dist/osc-browser.min.js'*/
 
-	// import osc from 'osc'
+	const source = new EventSource('/osc')
 
-	// console.log(osc)
-
-	if (!!window.EventSource) {
-	    var source = new EventSource('/countdown')
-
-	    source.addEventListener('message', function(e) {
-	      document.getElementById('data').innerHTML = e.data
-	    }, false)
-
-	    source.addEventListener('open', function(e) {
-	      document.getElementById('state').innerHTML = "Connected"
-	    }, false)
-
-	    source.addEventListener('error', function(e) {
-	      const id_state = document.getElementById('state')
-	      if (e.eventPhase == EventSource.CLOSED)
-	        source.close()
-	      if (e.target.readyState == EventSource.CLOSED) {
-	        id_state.innerHTML = "Disconnected"
-	      }
-	      else if (e.target.readyState == EventSource.CONNECTING) {
-	        id_state.innerHTML = "Connecting..."
-	      }
-	    }, false)
-	  } else {
-	    console.log("Your browser doesn't support SSE")
-	  }
-
+	source.addEventListener('message', e => {
+		console.log(e.data)
+	}, false)
 
 </script>
 
 <div id="container">
-	<h1>SSE: <span id="state"></span></h1>
-  	<h3>Data: <span id="data"></span></h3>
 	<div id="circle"></div>
 </div>
 
