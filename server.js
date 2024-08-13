b32c677d1d383a08df9aeabe0b72d5bf580cdb12
knaut <daniel@smallinvisiblemachines.com>
2024-08-13 01:29:11 -0700

    wip; using osc-min to send osc via udp

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import express from 'express'
 import * as osc from 'node-osc'
 
+import oscMin from 'osc-min'
+
+import dgram from 'dgram'
+
+const udp = dgram.createSocket('udp4')
+
 // console.log(osc)
 
 const app = express()
@@ -11,8 +17,58 @@ const vdmx = {
   port: 9000
 }
 
+const appPort = 41234
+
+
 const oscServer = new osc.Server(vdmx.port, vdmx.ip, () => {
   console.log('OSC server listening')
+})
+console.log(oscServer)
+
+const oscClient = new osc.Client(vdmx.ip, 41234)
+console.log(oscClient)
+
+const buf = oscMin.toBuffer({
+  address: '/test',
+  args: 1
+})
+
+// udp.send(buf, 0, buf.length, appPort, vdmx.ip)
+
+const fx = [
+  'interstellar sphere',
+  'organelles',
+  'black hole',
+  'triple rotate',
+  'hexagonal lattice',
+  'duo glitch',
+  'bizmuth',
+  'ascii iris'
+]
+
+oscServer.on('message', msg => {
+  console.log(msg)
+
+  const [ addr, val ] = msg
+
+  if (addr === '/fx/random_preset') {
+    const randIndex = Math.floor( Math.random() * fx.length)
+    const randPick = fx[ randIndex ]
+    const randAddr = '/overlay/fx_index'
+    
+    console.log(randAddr, randIndex)
+
+    // oscClient.send('/overlay/fx_index', randIndex, () => {})
+
+    const buf = oscMin.toBuffer({
+      address: randAddr,
+      args: randIndex
+    })
+
+    console.log(buf)
+
+    udp.send(buf, 0, buf.length, appPort, vdmx.ip)
+  }
 })
 
 app.get('/osc', function(req, res) {
@@ -42,4 +98,9 @@ function countdown(res, count) {
     res.end()
 }
 
-app.listen(3000, () => console.log('SSE app listening on port 3000!'))
+
+app.listen(appPort, () => console.log(`SSE app listening on port ${appPort}!`))
+
+
+
+
